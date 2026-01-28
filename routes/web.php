ce051d709888ed4bc2ef29e7dd36fb5243eebe87
EsmeraldaColiaza <160161475+EsmeraldaColiaza@users.noreply.github.com>
2026-01-27 19:53:34 -0600

    se agregó una funcion mas al archivo web.php

--- routes/web.php
+++ routes/web.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\PrincipalController;
 
 Route::get('/hello',HomeController::class);
 Route::get('/posts/mensaje', [PostController::class, 'Mensaje']);
-
+Route::get('post/about/{param?}/{name}', [PrincipalController::class, 'About']);
 
 
 
